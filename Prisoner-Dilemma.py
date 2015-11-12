b8a870052c1e5690e91b6208b29d2bbcf87e1bdc
Nightless <mbowles1205@lsr7.net>
2015-11-12 12:01:38 -0600

    Change team name of team 6

--- Prisoner-Dilemma.py
+++ Prisoner-Dilemma.py
@@ -250,7 +250,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 6:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Mb-NM'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
